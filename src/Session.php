7b92f7195740c3b32fa0001c6e6c0c3a53bfb56a
kris10ansn <telefon.kristian@gmail.com>
2021-01-25 12:58:59 +0100

    add Session::getUser method

--- src/Session.php
+++ src/Session.php
@@ -4,11 +4,28 @@
 namespace app\src;
 
 
+use app\models\User;
+
 class Session
 {
+    private static ?User $user = null;
+
     public static function start()
     {
         session_start();
+    }
+
+    public static function getUser(): ?User
+    {
+        if (self::$user === null) {
+            $userId = Session::get("user");
+
+            if ($userId !== null) {
+                self::$user = User::find(["id" => $userId]);
+            }
+        }
+
+        return self::$user;
     }
 
     public static function set($key, $value)
