560f68c48307433549a20cc5ed080a74b1e5f796
kris10ansn <telefon.kristian@gmail.com>
2021-01-25 12:59:26 +0100

    Session::get - return null if not set

--- src/Session.php
+++ src/Session.php
@@ -35,7 +35,7 @@ class Session
 
     public static function get($key)
     {
-        return $_SESSION[$key];
+        return $_SESSION[$key] ?? null;
     }
 
     public static function remove($key)
