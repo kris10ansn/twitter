d409f76049a32e7cb309712bc66154f44ef40df2
kris10ansn <telefon.kristian@gmail.com>
2021-01-26 10:10:11 +0100

    switch redirect fro history.back (javascript)

--- controllers/InteractionController.php
+++ controllers/InteractionController.php
@@ -30,6 +30,6 @@ class InteractionController extends \app\src\Controller
             die("403 Forbidden route");
         }
 
-        Response::redirect($previousUrl);
+        echo "<script>window.history.back();</script>";
     }
 }
