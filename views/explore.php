efb41e9b97c9039ebc80d446db7e800a739f05ac
BuildTools <unconfigured@null.spigotmc.org>
2021-02-03 20:49:24 +0100

    add sorted-by class to relevant button

--- views/explore.php
+++ views/explore.php
@@ -6,15 +6,25 @@
 use app\models\PostFormModel;
 use app\models\PostModel;
 use app\models\UserModel;
+use app\src\Request;
+
 ?>
 
 <link rel="stylesheet" href="styles/views/explore.css">
 
 <h1>Explore</h1>
 
+<?php
+function sortedBy(string $method): string {
+    $sort = Request::getParameter(Request::METHOD_GET, "sort");
+    $isSortedBy = $sort ? $sort === $method : $method === "top";
+    return $isSortedBy? "sorted-by" : "";
+}
+?>
+
 <form id="sort" method="get">
-    <button class="option" name="sort" value="top">Top</button>
-    <button class="option" name="sort" value="new">New</button>
+    <button class="option <?= sortedBy('top') ?>" name="sort" value="top">Top</button>
+    <button class="option <?= sortedBy('new') ?>" name="sort" value="new">New</button>
 </form>
 
 <?php include constant("APP_ROOT") . "/views/includes/posts.php" ?>
