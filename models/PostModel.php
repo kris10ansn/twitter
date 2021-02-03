e0276281e23e1aa0056aef9725d10170524566fe
BuildTools <unconfigured@null.spigotmc.org>
2021-02-03 21:03:13 +0100

    remove HashtagModel and replace with PostModel::withHashtag method

--- models/PostModel.php
+++ models/PostModel.php
@@ -120,6 +120,31 @@ class PostModel
         return $statement->fetchAll(\PDO::FETCH_CLASS, PostModel::class);
     }
 
+    public static function withHashtag(string $hashtag): array
+    {
+        $db = Database::getInstance();
+
+        $statement = $db->pdo->prepare("
+            SELECT
+                post.*, user.firstname, user.lastname, user.username,
+                (SELECT count(*) FROM `like` WHERE `like`.post_id=post.id AND `like`.user_id=:user_id) as liked,
+                (SELECT count(*) FROM `like` WHERE `like`.post_id=post.id) as likes
+            FROM hashtagged
+                JOIN post ON hashtagged.post_id = post.id
+                JOIN user ON post.user_id = user.id
+            WHERE name=:name
+            ORDER BY post.created_at
+        ");
+
+        $userId= Session::get("user");
+
+        $statement->bindValue(":name", $hashtag);
+        $statement->bindValue(":user_id", $userId);
+        $statement->execute();
+
+        return $statement->fetchAll(\PDO::FETCH_CLASS, PostModel::class);
+    }
+
     public function like(UserModel $user)
     {
         $db = Database::getInstance();
