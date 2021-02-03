57532d87b9fa034922023c5104c504ab82f2d64e
BuildTools <unconfigured@null.spigotmc.org>
2021-02-03 23:12:36 +0100

    update

--- views/user.php
+++ views/user.php
@@ -7,16 +7,21 @@ use app\src\Session;
 
 $me = Session::getUser();
 
+$follows = $me->follows($user->id);
 ?>
 
-<h1><?= "$user->firstname $user->lastname" ?> (<a href="user/<?= $user->id ?>">@<?= $user->username ?></a>)</h1>
+<link rel="stylesheet" href="styles/views/user.css">
 
-<?php if ($me && $user->id !== $me->id): ?>
-    <form action="follow/<?= $user->id ?>" method="post">
-        <button type="submit">
-            <?= !$me->follows($user->id) ? "Follow" : "Unfollow"; ?>
-        </button>
-    </form>
- <?php endif; ?>
+<div id="user">
+    <h1><?= "$user->firstname $user->lastname" ?> (<a href="user/<?= $user->id ?>">@<?= $user->username ?></a>)</h1>
+
+    <?php if ($me && $user->id !== $me->id): ?>
+        <form action="follow/<?= $user->id ?>" method="post">
+            <button type="submit" class="<?= $follows ? 'follows' : '' ?>">
+                <?= !$follows ? "Follow" : "Unfollow"; ?>
+            </button>
+        </form>
+    <?php endif; ?>
+</div>
 
 <?php include constant("APP_ROOT") . "/views/includes/posts.php" ?>
