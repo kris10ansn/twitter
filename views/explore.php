41cd8fb984e4429f18f2ab58a7c5b8423e7947ee
BuildTools <unconfigured@null.spigotmc.org>
2021-02-03 20:13:14 +0100

    update explore page

--- views/explore.php
+++ views/explore.php
@@ -8,6 +8,13 @@ use app\models\PostModel;
 use app\models\UserModel;
 ?>
 
+<link rel="stylesheet" href="styles/views/explore.css">
+
 <h1>Explore</h1>
+
+<form id="sort" method="get">
+    <button class="option" name="sort" value="top">Top</button>
+    <button class="option" name="sort" value="new">New</button>
+</form>
 
 <?php include constant("APP_ROOT") . "/views/includes/posts.php" ?>
