87611a5b4c949b2ef177cc0f5d2dc5d5f3e2f72e
BuildTools <unconfigured@null.spigotmc.org>
2021-02-04 23:27:33 +0100

    display followers and how many people the user follows

--- views/user.php
+++ views/user.php
@@ -13,15 +13,19 @@ $follows = $me->follows($user->id);
 <link rel="stylesheet" href="styles/views/user.css">
 
 <div id="user">
-    <h1><?= "$user->firstname $user->lastname" ?> (<a href="user/<?= $user->id ?>">@<?= $user->username ?></a>)</h1>
+    <div class="top">
+        <h1><?= "$user->firstname $user->lastname" ?> (<a href="user/<?= $user->id ?>">@<?= $user->username ?></a>)</h1>
 
-    <?php if ($me && $user->id !== $me->id): ?>
-        <form action="follow/<?= $user->id ?>" method="post">
-            <button type="submit" class="<?= $follows ? 'follows' : '' ?>">
-                <?= !$follows ? "Follow" : "Unfollow"; ?>
-            </button>
-        </form>
-    <?php endif; ?>
+        <?php if ($me && $user->id !== $me->id): ?>
+            <form action="follow/<?= $user->id ?>" method="post">
+                <button type="submit" class="<?= $follows ? 'follows' : '' ?>">
+                    <?= !$follows ? "Follow" : "Unfollow"; ?>
+                </button>
+            </form>
+        <?php endif; ?>
+    </div>
+
+    <p>Followers: <b><?= $user->followerCount() ?></b>&#9; Follows: <b><?= $user->followsCount() ?></b></p>
 </div>
 
 <?php include constant("APP_ROOT") . "/views/includes/posts.php" ?>
