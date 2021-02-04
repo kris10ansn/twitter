89e286e86c70bc1076644601c4e35df8e76bd51e
kris10ansn <telefon.kristian@gmail.com>
2021-02-04 09:44:54 +0100

    add Time utility class

---
+++ src/util/Time.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace app\src\util;
+
+
+class Time
+{
+    public static function since($time): string
+    {
+        $time = time() - $time;
+
+        $tokens = array (
+            365*24*60*60 => 'year',
+            30*24*60*60 => 'month',
+            7*24*60*60 => 'week',
+            24*60*60 => 'day',
+            60*60 => 'hour',
+            60 => 'minute',
+            1 => 'second'
+        );
+
+        $result = '';
+        $counter = 0;
+        foreach ($tokens as $unit => $text) {
+            if ($time < $unit) continue;
+            if ($counter > 1) break;
+
+            $numberOfUnits = floor($time / $unit);
+            $s = $numberOfUnits > 1 ? "s" : "";
+
+            $result .= "$numberOfUnits $text$s ";
+            $time -= $numberOfUnits * $unit;
+            ++$counter;
+        }
+
+        if ($result === "") {
+            return "Just now";
+        }
+
+        return "{$result}ago";
+    }
+}
