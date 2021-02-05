1825d424752c7b6165d6c0edaf5008656e6fd629
kris10ansn <telefon.kristian@gmail.com>
2021-02-05 09:13:15 +0100

    use BigInputComponent

--- views/home.php
+++ views/home.php
@@ -9,36 +9,18 @@ use app\models\PostFormModel;
 use app\models\PostModel;
 use app\models\UserModel;
 use app\src\Session;
+use app\views\components\BigInputComponent;
 
 $user = Session::getUser();
 ?>
 
-<script>
-    function onSubmit() {
-        const textarea = document.querySelector("#new-post textarea");
-        const contentEditable = document.querySelector("#new-post div#text-input");
-
-        textarea.value = contentEditable.textContent;
-    }
-</script>
-
-<?php if ($user !== null): ?>
+<?php if ($user !== null): ?>   
     <h1>Following</h1>
 <?php endif; ?>
 
-<div id="new-post" class="card">
-    <?php if ($user !== null): ?>
-        <form action="" method="post" onsubmit="onSubmit()">
-            <div id="input">
-                <div contenteditable data-placeholder="What's on your mind?" id="text-input"><?= $postFormModel->fields["text"] ?></div>
-                <button type="submit">Post</button>
-            </div>
-            <!-- PHPStorm klager på at jeg ikke har label element rundt textarea.. -->
-            <label>
-                <textarea name="text"></textarea>
-            </label>
-            <p class="error"><?= $postFormModel->getFirstError("text") ?></p>
-        </form>
+<div id="top-card" class="card">
+    <?php if ($user != null): ?>
+        <?= new BigInputComponent($postFormModel, "What&#39;s on your mind?") ?>
     <?php else: ?>
         <p><a href="login">Log in</a> to post and follow users. <a href="explore">Explore</a>.</p>
     <?php endif; ?>
