d0b8985505b14cb2d5b2ca6240cb2def886f5028
kris10ansn <telefon.kristian@gmail.com>
2021-02-05 09:13:03 +0100

    add BigInputComponent

---
+++ views/components/BigInputComponent.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace app\views\components;
+
+
+use app\models\FormModel;
+
+class BigInputComponent
+{
+    private FormModel $formModel;
+    private string $placeholder;
+    private string $action;
+    private int $identifier;
+
+    public function __construct(FormModel $formModel, string $placeholder, string $action="")
+    {
+        $this->formModel = $formModel;
+        $this->placeholder = $placeholder;
+        $this->action = $action;
+
+        $this->identifier = rand(0, PHP_INT_MAX);
+    }
+
+    public function __toString(): string
+    {
+        return "
+        <script>
+            function onSubmit{$this->identifier}() {
+                const textarea = document.querySelector('#{$this->identifier} textarea');
+                const contentEditable = document.querySelector('#{$this->identifier} div.text-input');
+                textarea.value = contentEditable.textContent;
+            }
+        </script>
+        <div id='{$this->identifier}' class='big-input'>
+            <form action='{$this->action}' method='post' onsubmit='onSubmit{$this->identifier}()'>
+                <div class='input'>
+                    <div contenteditable data-placeholder='{$this->placeholder}' class='text-input'>{$this->formModel->fields['text']}</div>
+                    <button type='submit'>Post</button>
+                </div>
+                <textarea name='text'></textarea>
+                <p class='error'>{$this->formModel->getFirstError('text')}</p>
+            </form>
+        </div>
+        ";
+    }
+}
+?>
+<link rel="stylesheet" href="styles/components/big-input.css">
+
