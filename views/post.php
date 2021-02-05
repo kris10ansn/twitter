3e158ecef53511647b7b7cd0066deed5371ccbf1
kris10ansn <telefon.kristian@gmail.com>
2021-02-05 10:42:37 +0100

    add comment functionality and comments view

--- views/post.php
+++ views/post.php
@@ -1,7 +1,10 @@
 <?php
 /** @var PostModel $post */
+/** @var PostFormModel $postFormModel */
 
+use app\models\PostFormModel;
 use app\models\PostModel;
+use app\views\components\BigInputComponent;
 use app\views\components\PostComponent;
 
 ?>
@@ -9,3 +12,9 @@ use app\views\components\PostComponent;
 <link rel="stylesheet" href="styles/components/post.css">
 
 <?= new PostComponent($post, false) ?>
+
+<div class="card">
+    <?= new BigInputComponent($postFormModel, "Reply to @{$post->username}&#39;s post") ?>
+</div>
+
+<?php include constant("APP_ROOT") . "/views/includes/posts.php" ?>
