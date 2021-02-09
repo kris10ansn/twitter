4503cdb3417fb8ada8b8661f6b46be13fab44626
kris10ansn <telefon.kristian@gmail.com>
2021-02-09 08:54:43 +0100

    use RegexRule, merge username rules with nameRules

--- models/RegisterFormModel.php
+++ models/RegisterFormModel.php
@@ -9,6 +9,7 @@ use app\src\Session;
 use app\src\validation\EmailRule;
 use app\src\validation\MaximumLengthRule;
 use app\src\validation\MinimumLengthRule;
+use app\src\validation\RegexRule;
 use app\src\validation\RequiredRule;
 use app\src\validation\UniqueRule;
 
@@ -41,7 +42,12 @@ class RegisterFormModel extends FormModel
 
     protected function rules(): array
     {
-        $nameRules = [new RequiredRule(), new MinimumLengthRule(2), new MaximumLengthRule(45)];
+        $nameRules = [
+            new RequiredRule(),
+            new MinimumLengthRule(2),
+            new MaximumLengthRule(45),
+            new RegexRule("\w+")
+        ];
 
         return [
             "email" => [
@@ -54,16 +60,9 @@ class RegisterFormModel extends FormModel
                     "A user with that e-mail already exists"
                 ),
             ],
-            "username" => [
-                new RequiredRule(),
-                new MinimumLengthRule(2),
-                new MaximumLengthRule(45),
-                new UniqueRule(
-                    "user",
-                    "username",
-                    "A user with that username already exists"
-                ),
-            ],
+            "username" => array_merge($nameRules, [
+                new UniqueRule("user", "username", "A user with that username already exists")
+            ]),
             "firstname" => $nameRules,
             "lastname" => $nameRules,
             "password" => [new RequiredRule(), new MinimumLengthRule(4), new MaximumLengthRule(96)],
