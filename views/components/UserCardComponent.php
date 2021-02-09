c85bd3116466ae44455eea8af966bac57063c7c9
kris10ansn <telefon.kristian@gmail.com>
2021-02-09 12:15:02 +0100

    add UserCardComponent

---
+++ views/components/UserCardComponent.php
@@ -0,0 +1,57 @@
+<?php
+
+
+namespace app\views\components;
+
+
+use app\models\UserModel;
+use app\src\Session;
+use app\src\util\Time;
+
+class UserCardComponent
+{
+    private UserModel $user;
+
+    public function __construct(UserModel $user)
+    {
+        $this->user = $user;
+    }
+
+    public function __toString(): string
+    {
+        $me = Session::getUser();
+
+        $follows = $me !== null && $me->follows($this->user->id);
+        $followsClass = $follows ? "follows" : "";
+        $followButtonText = $follows ? "Unfollow" : "Follow";
+
+        $timeSinceCreation = Time::since(strtotime($this->user->created_at));
+
+        return "
+            <div class='card user'>
+                <div class='top'>
+                    <div class='user-info'>
+                        <p>
+                            <b>{$this->user->firstname} {$this->user->lastname}</b>
+                            (<a href='user/{$this->user->id}'>@{$this->user->username}</a>)
+                        </p>
+                        <small>
+                            <b>{$this->user->followerCount()}</b> followers
+                            <span class='gray'>Joined {$timeSinceCreation}</span>
+                        </small>
+                    </div>
+                    <form action='follow/{$this->user->id}' method='post'>
+                        <button class='{$followsClass}'>
+                            {$followButtonText}
+                        </button>
+                    </form>
+                </div>
+                <div class='biography'>
+                    <p>{$this->user->biography}</p>
+                </div>
+            </div>";
+    }
+}
+?>
+<link rel="stylesheet" href="styles/components/user.css">
+
