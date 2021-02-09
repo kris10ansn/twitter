429fbb06221c71e3da9bd20a5f89f4f80334000c
kris10ansn <telefon.kristian@gmail.com>
2021-02-09 12:15:11 +0100

    use UserCardComponent

--- views/users.php
+++ views/users.php
@@ -4,41 +4,17 @@
 
 use app\models\UserModel;
 use app\src\Session;
-use app\src\util\Time;
+use app\views\components\UserCardComponent;
 
 $me = Session::getUser();
 $loggedIn = $me !== null;
 
 ?>
 
-<link rel="stylesheet" href="styles/components/user.css">
-
 <h1>Users <?= $loggedIn ? "to follow" : "" ?></h1>
 
 <?php include constant("APP_ROOT") . "/views/includes/sort.php" ?>
 
 <?php foreach ($users as $user): ?>
-    <div class="card user">
-        <div class="top">
-            <div class="user-info">
-                <p>
-                    <b><?= "$user->firstname $user->lastname" ?></b>
-                    (<a href="user/<?= $user->id ?>">@<?= $user->username ?></a>)
-                </p>
-                <small>
-                    <b><?= $user->followerCount() ?></b> followers
-                    <span class="gray">Joined <?= Time::since(strtotime($user->created_at)) ?></span>
-                </small>
-            </div>
-            <form action="follow/<?= $user->id ?>" method="post">
-                <?php $follows = $loggedIn && $me->follows($user->id); ?>
-                <button class="<?= $follows ? "follows" : "" ?>">
-                    <?= $follows ? "Unfollow" : "Follow" ?>
-                </button>
-            </form>
-        </div>
-        <div class="biography">
-            <?= $user->biography ? "<p>$user->biography</p>" : "" ?>
-        </div>
-    </div>
+    <?= new UserCardComponent($user) ?>
 <?php endforeach; ?>
